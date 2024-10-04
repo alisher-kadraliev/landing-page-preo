2ad8b29df4564b9fcd871d5c0870cd22605f0da3
Alisher Kadraliev <alisherkadraliev@gmail.com>
2024-10-04 12:05:12 +0300

    popup added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#eaeefe]`}
       >
         {children}
-        <script src="https://moonpix.vercel.app/popup.js"></script>
+        <Script src="https://moonpix.vercel.app/popup.js"></Script>
       </body>
     </html>
   );
